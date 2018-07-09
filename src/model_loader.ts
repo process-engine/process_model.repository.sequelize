6d3249316e3bd4b589a4289d4d83e8e3e38f1311
ElRaptorus <christian.werner@5minds.de>
2018-07-09 16:59:11 +0200

    Add repository- and model-loader scaffolding

---
+++ src/model_loader.ts
@@ -0,0 +1,3 @@
+export async function loadModels(config: any): Promise<void> {
+  return Promise.resolve();
+}
---
+++ src/process_model_instane_repository.ts
@@ -0,0 +1,16 @@
+import {Definitions, IProcessModelRepository, Model} from '@process-engine/process_engine_contracts';
+
+export class ProcessModelRepository implements IProcessModelRepository {
+
+  public async persistProcessDefinitions(definitions: Definitions): Promise<void> {
+    throw new Error('Not implemented.');
+  }
+
+  public async getProcessModelById(processModelId: string): Promise<Model.Types.Process> {
+    throw new Error('Not implemented.');
+  }
+
+  public async getProcessModels(): Promise<Array<Model.Types.Process>> {
+    throw new Error('Not implemented.');
+  }
+}
