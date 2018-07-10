31dde6fc01f7d5867a2a08948de7f48c3fba7a51
ElRaptorus <christian.werner@5minds.de>
2018-07-10 13:26:26 +0200

    Implement repository initialization

--- src/process_model_repository.ts
+++ src/process_model_repository.ts
@@ -1,6 +1,38 @@
 import {Definitions, IProcessModelFromRepository, IProcessModelRepository, Model} from '@process-engine/process_engine_contracts';
 
+import {getConnection} from '@essential-projects/sequelize_connection_manager';
+
+import * as Sequelize from 'sequelize';
+
+import {loadModels} from './model_loader';
+import {IProcessModelAttributes, ProcessModel} from './schemas';
+
 export class ProcessModelRepository implements IProcessModelRepository {
+
+  public config: any;
+  // {
+  //   "username": "admin",
+  //   "password": "admin",
+  //   "database": "processengine",
+  //   "host": "localhost",
+  //   "port": 45678,
+  //   "dialect": "postgres",
+  //   "supportBigNumbers": true,
+  //   "resetPasswordRequestTimeToLive": 12
+  // }
+
+  private _processModel: Sequelize.Model<ProcessModel, IProcessModelAttributes>;
+
+  private sequelize: Sequelize.Sequelize;
+
+  private processModel(): Sequelize.Model<ProcessModel, IProcessModelAttributes> {
+    return this._processModel;
+  }
+
+  public async initialize(): Promise<void> {
+    this.sequelize = await getConnection(this.config.database, this.config.username, this.config.password, this.config);
+    this._processModel = await loadModels(this.sequelize);
+  }
 
   public async persistProcessDefinitions(definitions: Definitions): Promise<void> {
     throw new Error('Not implemented.');
