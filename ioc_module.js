5108e0d41ad370e82971f9a793ef9ec142b41e9b
ElRaptorus <christian.werner@5minds.de>
2018-12-10 10:27:50 +0100

    :sparkles: Add disposable tag to ioc registration

--- ioc_module.js
+++ ioc_module.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const ProcessDefinitionRepository = require('./dist/commonjs/index').ProcessDefinitionRepository;
+const disposableDiscoveryTag = require('@essential-projects/bootstrapper_contracts').disposableDiscoveryTag;
 
 function registerInContainer(container) {
 
   container.register('ProcessDefinitionRepository', ProcessDefinitionRepository)
     .dependencies('SequelizeConnectionManager')
     .configure('process_engine:process_model_repository')
+    .tags(disposableDiscoveryTag)
     .singleton();
 }
 
