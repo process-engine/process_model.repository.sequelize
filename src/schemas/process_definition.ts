25914b079f0d4c3b20a4c7bcd939256d9ed03ec0
ElRaptorus <christian.werner@5minds.de>
2019-04-30 13:04:30 +0200

    :fire: Disable automatic versioning

--- src/schemas/process_definition.ts
+++ src/schemas/process_definition.ts
@@ -1,6 +1,6 @@
 import {AllowNull, Column, CreatedAt, DataType, Model, Table, UpdatedAt} from 'sequelize-typescript';
 
-@Table({modelName: 'ProcessDefinition', tableName: 'ProcessDefinitions', version: true})
+@Table({modelName: 'ProcessDefinition', tableName: 'ProcessDefinitions'})
 export class ProcessDefinitionModel extends Model<ProcessDefinitionModel> {
 
   @AllowNull(false)
